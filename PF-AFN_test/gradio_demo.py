c52f668aba892100c2d8f7360dee64ae59929af7
Pranav Gadikar <pranav.gayu@gmail.com>
2023-05-28 20:34:26 +0000

    small changes

--- PF-AFN_test/gradio_demo.py
+++ PF-AFN_test/gradio_demo.py
@@ -32,7 +32,7 @@ def get_combined_image(real_image, clothes, tryon):
     a = real_image.float().cuda()
     b = clothes.cuda()
     c = tryon
-    combine = torch.cat([a[0], b[0], c[0]], 2).squeeze()
+    combine = torch.cat([c[0]], 2).squeeze()
     cv_img = (combine.permute(1, 2, 0).detach().cpu().numpy() + 1) / 2
     rgb = (cv_img * 255).astype(np.uint8)
     # bgr = cv2.cvtColor(rgb, cv2.COLOR_RGB2BGR)
